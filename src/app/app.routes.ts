92264c9e471f56c98ff20ffd75ee136f1277a25c
omar dorbaa <omar.dev@epsilonsoft.tech>
2025-01-26 20:10:24 +0400

    add layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AccountComponent } from './account/account.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'account',
+        component: AccountComponent,
+      },
+      //   {
+      //     path: 'child-b',
+      //     component: ChildBComponent,
+      //   },
+    ],
+  },
+];
