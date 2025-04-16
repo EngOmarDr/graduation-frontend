491d04a8456f42ca3f48ea1e788dbb380df7257f
hamza23177 <hamzamhamd23177@gmail.com>
2025-04-16 01:40:14 -0700

    add product

--- src/app/product/add-product/add-product.component.ts
+++ src/app/product/add-product/add-product.component.ts
@@ -1,29 +1,61 @@
 import { Component, inject } from '@angular/core';
 import { CardComponent } from '../../../components/card-form.component';
 import { CustomFieldComponent } from '../../../components/custom-field.component';
-import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators ,FormBuilder, FormGroup } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
+
 @Component({
   selector: 'app-add-product',
   imports: [
-    CardComponent,
-    CustomFieldComponent,
     ReactiveFormsModule,
-    CommonModule,
-  ],
+    CommonModule
+],
   templateUrl: './add-product.component.html',
 })
 export class AddProductComponent {
-  private fb = inject(NonNullableFormBuilder);
+  stockForm!: FormGroup;
 
-  form = this.fb.group({
-    name: ['', [Validators.required]],
-    phone: [''],
-    address: [''],
-    note: [''],
-  });
+  constructor(private fb: FormBuilder) {}
 
-  onSubmit() {
-    alert(this.form.valid);
+  ngOnInit(): void {
+    this.stockForm = this.fb.group({
+      name: ['', Validators.required],
+      sku: ['', Validators.required],
+      productImages: [null],
+      category: ['', Validators.required],
+      brand: ['', Validators.required],
+      barcodeType: ['', Validators.required],
+      productUnit: ['', Validators.required],
+      unitOfSale: ['', Validators.required],
+      additionalUnit: [''],
+      status: ['', Validators.required],
+      quantityLimit: [''],
+      notes: [''],
+      productType: ['', Validators.required],
+      productPrice: ['', Validators.required],
+      productCost: ['', Validators.required],
+      taxType: [''],
+      addedQuantity: [''],
+      stockAlert: [''],
+      orderTax: [''],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.stockForm.valid) {
+      console.log('Form Submitted:', this.stockForm.value);
+    } else {
+      this.stockForm.markAllAsTouched();
+    }
+  }
+
+  onFileChange(event: any): void {
+    const files = event.target.files;
+    if (files.length > 0) {
+      this.stockForm.patchValue({
+        productImages: files
+      });
+    }
   }
 }
