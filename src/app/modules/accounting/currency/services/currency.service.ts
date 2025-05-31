0d338b6f3c95d3e68885f2b983bbb90d5ebd8024
omar dorbaa <omar.dev@epsilonsoft.tech>
2025-05-31 14:02:19 +0400

    replace import environments.development with environments

--- src/app/modules/accounting/currency/services/currency.service.ts
+++ src/app/modules/accounting/currency/services/currency.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Currency } from '../models/currency.model';
-import { environment } from '../../../../../environments/environment.development';
+import { environment } from '../../../../../environments/environment';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
